4ad34cf107d3eebe947f9713be7ee1e9993ed5c0
Tyler Stevenson <stevetylda@gmail.com>
2026-01-31 16:41:56 -0500

    Adjusted vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   base: mode === "production" ? "/OrcaCast_App/" : "/",
+//   plugins: [react()],
+// }));
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/OrcaCast_App/" : "/",
+export default defineConfig(() => ({
+  base: "/",
   plugins: [react()],
 }));
